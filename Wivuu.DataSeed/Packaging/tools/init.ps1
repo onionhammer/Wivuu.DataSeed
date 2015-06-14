68fbaab0e7163c92bd000d60d349c7bcd2d80fc2
Erik O'Leary <erik.m.oleary@gmail.com>
2015-06-13 23:23:59 -0500

    Started setting up scaffolding scripts

--- Wivuu.DataSeed/Packaging/tools/init.ps1
+++ Wivuu.DataSeed/Packaging/tools/init.ps1
@@ -1,6 +1,5 @@
-## Module Functions
-# enable-data-migrations
-#   Creates a new empty migration, extending from InitialDataMigration class
-#
-# add-data-migration name
-#   Creates a stub migration class, extending from DataMigration<T>
+# Ref: http://haacked.com/archive/2011/04/19/writing-a-nuget-package-that-adds-a-command-to-the.aspx/
+
+param($installPath, $toolsPath, $package)
+
+Import-Module (Join-Path $toolsPath DataSeed.psm1) -DisableNameChecking
