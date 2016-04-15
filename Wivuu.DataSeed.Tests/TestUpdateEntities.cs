c3853560f8debb8fc48c59241b819bc88af0daab
Erik O'Leary <erik.m.oleary@gmail.com>
2016-04-15 18:40:27 -0500

    - Additional test scenario covered

--- Wivuu.DataSeed.Tests/TestUpdateEntities.cs
+++ Wivuu.DataSeed.Tests/TestUpdateEntities.cs
@@ -20,6 +20,7 @@ namespace Wivuu.DataSeed.Tests
         [TestMethod]
         public void TestDefaults()
         {
+            // Scenario 1
             var item0 = new TestUser
             {
                 Id   = new Guid("27a21f77-c286-4767-a23c-571ec4d11aee"),
@@ -36,6 +37,7 @@ namespace Wivuu.DataSeed.Tests
 
             Assert.AreEqual("Craig Smith", updated0.Name);
 
+            // Scenario 2
             var item1 = default(TestUser);
 
             var updated1 = item1.Update(
@@ -48,6 +50,22 @@ namespace Wivuu.DataSeed.Tests
             });
 
             Assert.AreEqual("Craig Smith", updated1.Name);
+
+            // Scenario 3
+            var item2 = default(TestUser);
+
+            var updated2 = item2
+                .Update(new { Name = "Craig Smith" })
+                .Update(new { Name = "Jim Smith" })
+                .Default(() =>
+                {
+                    var c = new TestUser();
+                    c.Id = item0.Id;
+                    return c;
+                });
+
+            Assert.AreEqual("Jim Smith", updated2.Name);
+
         }
     }
 }
